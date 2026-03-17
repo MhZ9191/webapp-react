f8db862839f2ab5f1b95132cb51929a46487925b
MhZ9191 <hegic91@gmail.com>
2026-03-17 15:55:54 +0100

    Add connect movie page to detail page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Card({ element }) {
   const { title, genre, abstract, image } = element;
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      <p>{genre}</p>
-      <p>{abstract}</p>
-      <img src={image} alt={title} />
-    </div>
+    <Link to={`/movies/${element.id}`} className="div-main-card">
+      <div className="card">
+        <h2>{title}</h2>
+        <p>{genre}</p>
+        <p>{abstract}</p>
+        <img src={image} alt={title} />
+      </div>
+    </Link>
   );
 }
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 export default function DetailPage() {
+  const { id } = useParams();
+
   return (
     <>
-      <h2>Detail</h2>
+      <h2>Detail movie {id}</h2>
     </>
   );
 }
