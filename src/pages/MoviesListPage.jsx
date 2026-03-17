4b5d5aedca2b8817e90d4b38198d850cc5a11135
MhZ9191 <hegic91@gmail.com>
2026-03-17 13:45:26 +0100

    Add axios call

--- src/pages/MoviesListPage.jsx
+++ src/pages/MoviesListPage.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function MovieslLisPage() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    axios.get("http://localhost:3000/movies").then((res) => {
+      setMovies(res.data.results);
+    });
+  };
+
+  useEffect(fetchMovies, []);
+
   return (
     <>
       <h2>List</h2>
+      <div>
+        {movies.map((el) => {
+          return <p key={el.id}>{el.title}</p>;
+        })}
+      </div>
     </>
   );
 }
