11672b04aa4ff5ce261e25a4f375c101c9d196fe
MhZ9191 <hegic91@gmail.com>
2026-03-17 17:08:37 +0100

    Add move img in assets

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,14 @@ export default function Card({ element }) {
   return (
     <Link to={`/movies/${element.id}`} className="div-main-card">
       <div className="card">
-        <h2>{title}</h2>
-        <p>{genre}</p>
-        <p>{abstract}</p>
-        <img src={image} alt={title} />
+        <div className="sub-card">
+          <h2>{title}</h2>
+          <p>{genre}</p>
+          <p>{abstract}</p>
+        </div>
+        <div className="card-image">
+          <img src={image} alt={title} />
+        </div>
       </div>
     </Link>
   );
