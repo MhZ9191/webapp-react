15782039d7aa8a05e68153670b32985e88e54e6e
MhZ9191 <hegic91@gmail.com>
2026-03-17 16:02:21 +0100

    Add axios call DetailPage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,11 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function DetailPage() {
   const { id } = useParams();
 
+  const [detail, setDetail] = useState({});
+
+  const fetchDetail = () => {
+    axios.get("http://localhost:3000/movies/" + id).then((res) => {
+      setDetail(res.data.result);
+    });
+  };
+
+  useEffect(fetchDetail, []);
+
   return (
     <>
-      <h2>Detail movie {id}</h2>
+      <h2>Detail movie {detail.title}</h2>
+      <img src={detail.image} alt={detail.title} />
     </>
   );
 }
