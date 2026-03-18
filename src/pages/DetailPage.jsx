22dd1fa65427882450e182a1b02b4b4e10f0f5ff
MhZ9191 <hegic91@gmail.com>
2026-03-18 16:50:45 +0100

    Add reviews

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -16,9 +16,45 @@ export default function DetailPage() {
   useEffect(fetchDetail, []);
 
   return (
-    <>
-      <h2>Detail movie {detail.title}</h2>
-      <img src={detail.image} alt={detail.title} />
-    </>
+    <main>
+      <section className="main-sec-detail">
+        <div className="main-div-detail">
+          <div className="div-image-detail">
+            <img src={detail.image} alt={detail.title} />
+          </div>
+          <div>
+            <h2>Title</h2>
+            <span>{detail.title}</span>
+            <h2>Genre</h2>
+            <span>{detail.genre}</span>
+            <h2>Abstract</h2>
+            <span>{detail.abstract}</span>
+            <h2>Release Year</h2>
+            <span>{detail.release}</span>
+          </div>
+        </div>
+        <div className="div-rev">
+          {detail.reviews &&
+            detail.reviews.map((el) => {
+              return (
+                <div className="div-reviews">
+                  <div>{el.name}</div>
+                  <div>{el.text}</div>
+                  <div>{createStar(el.vote, 5)}</div>
+                </div>
+              );
+            })}
+        </div>
+      </section>
+    </main>
   );
+}
+
+function createStar(vote, maxVote) {
+  let test = [];
+
+  for (let i = 1; i <= maxVote; i++) {
+    test[i - 1] = i <= vote ? <span>&#x2605;</span> : <span>&#x2606;</span>;
+  }
+  return test;
 }
