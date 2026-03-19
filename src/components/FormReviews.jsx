ced3279c4513f5b6131552bf016104e14360e9b1
MhZ9191 <hegic91@gmail.com>
2026-03-19 17:45:22 +0100

    Add css form

--- src/components/FormReviews.jsx
+++ src/components/FormReviews.jsx
@@ -29,31 +29,37 @@ export default function FormReviews({ movie_id, fetch }) {
       <fieldset>
         <legend>Add New Reviews</legend>
         <form onSubmit={submitForm} className="form-submit">
-          <label htmlFor="name-user">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="name-user"
-            onChange={updateRev}
-            value={newReviews.name}
-          />
-          <label htmlFor="textarea-form">Text</label>
-          <textarea
-            name="text"
-            id="textarea-form"
-            onChange={updateRev}
-            value={newReviews.text}
-          ></textarea>
-          <label htmlFor="vote-form">Vote</label>
-          <input
-            type="number"
-            name="vote"
-            value={newReviews.vote}
-            onChange={updateRev}
-            min="1"
-            max="5"
-            id="vote-form"
-          />
+          <div>
+            <label htmlFor="name-user">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name-user"
+              onChange={updateRev}
+              value={newReviews.name}
+            />
+          </div>
+          <div>
+            <label htmlFor="textarea-form">Text</label>
+            <textarea
+              name="text"
+              id="textarea-form"
+              onChange={updateRev}
+              value={newReviews.text}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="vote-form">Vote</label>
+            <input
+              type="number"
+              name="vote"
+              value={newReviews.vote}
+              onChange={updateRev}
+              min="1"
+              max="5"
+              id="vote-form"
+            />
+          </div>
           <button>Send</button>
         </form>
       </fieldset>
